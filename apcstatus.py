0b1d4f5767510ecb02fc2c2896cd6d306057dae7
Dhesant Nakka <dhesant@gmail.com>
2018-04-26 19:45:20 +0800

    initial commit; influxdb line protocol

---
+++ apcstatus.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import subprocess
+import dateutil.parser as dparser
+import pytz
+
+# Holding Variable
+status = {}
+
+# Key defintions
+ignore = ['apc', 'cable', 'driver', 'upsmode', 'starttime', 'end apc']
+tags = ['hostname', 'upsname', 'serialno', 'status']
+date_parse = ['date', 'battdate', 'xonbatt', 'xoffbatt']
+force_text = ['version', 'serialno', 'firmware', 'statflag']
+
+# Parse output into holding variable
+res = subprocess.getoutput(["/sbin/apcaccess -u"])
+for line in res.split("\n"):
+    (key,spl,val) = line.partition(': ')
+    key = key.strip().lower()
+    val = val.strip()
+    if key not in ignore:
+        if val[0].isdigit() and key not in force_text:
+            if key in date_parse:
+                status[key] = dparser.parse(val)
+            else:
+                status[key] = float(val)
+        else:
+            status[key] = val
+
+# Generate InfluxDB Line Protocol String        
+influx_string = 'apcups'
+
+# Add Influx Tags
+for tag in tags:
+    influx_string = influx_string + ',' + tag + '=' + status[tag]
+
+influx_string = influx_string + ' '
+
+# Add Inflix Measurements
+for key in status:
+    if key not in tags and key != 'date':
+        influx_string = influx_string + key + '='
+        if type(status[key]) is str:
+            influx_string = influx_string + '"' + status[key] + '"'
+        elif key in date_parse:
+            influx_string = influx_string + str(int(status[key].timestamp()))
+        else:
+            influx_string = influx_string + str(status[key])
+        influx_string = influx_string + ','
+
+influx_string = influx_string[:-1] + ' ' + str(int(status['date'].timestamp() * 1000000000))
+
+#print(status)
+print(influx_string)
